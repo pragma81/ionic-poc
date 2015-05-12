8fd995176cdbb5cb8ff68e1165f79dc970525670
Davide Antelmo <davide.antelmo@gmail.com>
2015-05-12 11:05:55 +0200

    first branch commit

--- www/js/app.js
+++ www/js/app.js
@@ -18,22 +18,141 @@ var app = angular.module('starter', ['ionic','ngStorage','ngCordova'])
   });
 })
 
- app.config(function($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('login', {
-                url: '/login',
+ app.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+
+     // Turn off caching for demo simplicity's sake
+    $ionicConfigProvider.views.maxCache(0);
+
+    /*
+    // Turn off back button text
+    $ionicConfigProvider.backButton.previousTitleText(false);
+    */
+
+    $stateProvider.state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'main-content.html',
+        controller: 'AppCtrl'
+    })
+
+     .state('app.login', {
+        url: '/login',
+        views: {
+            'menuContent': {
                 templateUrl: 'js/login/login.html',
                 controller: 'LoginController'
-            })
-            .state('profile', {
-                url: '/profile',
+            },
+            'fabContent': {
+                template: ''
+            }
+        }
+    })
+
+    .state('app.profile', {
+        url: '/profile',
+        views: {
+            'menuContent': {
                 templateUrl: 'js/profile/profile.html',
                 controller: 'ProfileController'
-            })
-            .state('feed', {
-                url: '/feed',
-                templateUrl: 'js/feed/feed.html',
-                controller: 'FeedController'
-            });
-    $urlRouterProvider.otherwise('/login');
+            },
+            'fabContent': {
+                template: '<button id="fab-profile" class="button button-fab button-fab-bottom-right button-energized-900"><i class="icon ion-plus"></i></button>',
+                controller: function ($timeout) {
+                    /*$timeout(function () {
+                        document.getElementById('fab-profile').classList.toggle('on');
+                    }, 800);*/
+                }
+            }
+        }
+    })
+    ;
+
+
+    $urlRouterProvider.otherwise('/app/login');
     });
+
+
+//Main controller
+app.controller('AppCtrl', function($scope, $ionicModal, $ionicPopover, $timeout) {
+    // Form data for the login modal
+    $scope.loginData = {};
+    $scope.isExpanded = false;
+    $scope.hasHeaderFabLeft = false;
+    $scope.hasHeaderFabRight = false;
+
+    var navIcons = document.getElementsByClassName('ion-navicon');
+    for (var i = 0; i < navIcons.length; i++) {
+        navIcons.addEventListener('click', function() {
+            this.classList.toggle('active');
+        });
+    }
+
+    ////////////////////////////////////////
+    // Layout Methods
+    ////////////////////////////////////////
+
+    $scope.hideNavBar = function() {
+        document.getElementsByTagName('ion-nav-bar')[0].style.display = 'none';
+    };
+
+    $scope.showNavBar = function() {
+        document.getElementsByTagName('ion-nav-bar')[0].style.display = 'block';
+    };
+
+    $scope.noHeader = function() {
+        var content = document.getElementsByTagName('ion-content');
+        for (var i = 0; i < content.length; i++) {
+            if (content[i].classList.contains('has-header')) {
+                content[i].classList.toggle('has-header');
+            }
+        }
+    };
+
+    $scope.setExpanded = function(bool) {
+        $scope.isExpanded = bool;
+    };
+
+    $scope.setHeaderFab = function(location) {
+        var hasHeaderFabLeft = false;
+        var hasHeaderFabRight = false;
+
+        switch (location) {
+            case 'left':
+                hasHeaderFabLeft = true;
+                break;
+            case 'right':
+                hasHeaderFabRight = true;
+                break;
+        }
+
+        $scope.hasHeaderFabLeft = hasHeaderFabLeft;
+        $scope.hasHeaderFabRight = hasHeaderFabRight;
+    };
+
+    $scope.hasHeader = function() {
+        var content = document.getElementsByTagName('ion-content');
+        for (var i = 0; i < content.length; i++) {
+            if (!content[i].classList.contains('has-header')) {
+                content[i].classList.toggle('has-header');
+            }
+        }
+
+    };
+
+    $scope.hideHeader = function() {
+        $scope.hideNavBar();
+        $scope.noHeader();
+    };
+
+    $scope.showHeader = function() {
+        $scope.showNavBar();
+        $scope.hasHeader();
+    };
+
+    $scope.clearFabs = function() {
+        var fabs = document.getElementsByClassName('button-fab');
+        if (fabs.length && fabs.length > 1) {
+            fabs[0].remove();
+        }
+    };
+})
--- www/js/login/LoginController.js
+++ www/js/login/LoginController.js
@@ -1,6 +1,13 @@
 angular.module('starter').controller('LoginController', LoginController)
 
-function LoginController($scope, $cordovaOauth, $localStorage, $location) {
+function LoginController($scope, $timeout,$stateParams,$cordovaOauth, $localStorage, $location) {
+
+    $scope.$parent.clearFabs();
+    $timeout(function() {
+        $scope.$parent.hideHeader();
+    }, 0);
+    ionic.material.ink.displayEffect();
+
 
     $scope.login = function() {
         $cordovaOauth.facebook("823607484376036", ["email", "read_stream", "user_website", "user_location", "user_relationships"]).then(function(result) {
